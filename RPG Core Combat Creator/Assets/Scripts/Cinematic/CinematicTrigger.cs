2020f05f0ea9e4b647ec6cd9af994a760e4bfa54
Serge Maia <sergemaia9@gmail.com>
2021-12-09 16:44:58 +0000

    ADD: Cut Scenes

---
+++ RPG Core Combat Creator/Assets/Scripts/Cinematic/CinematicTrigger.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine.Playables;
+using UnityEngine;
+
+namespace RPG.Cinematic {
+
+    public class CinematicTrigger : MonoBehaviour {
+
+        private bool alreadyTriggered = false;
+
+        private void OnTriggerExit (Collider other) {
+            if(!alreadyTriggered && other.gameObject.tag == "Player") {
+                alreadyTriggered = true;
+                GetComponent<PlayableDirector> ().Play ();
+            }
+                
+        } // OnTriggerExit
+
+    } // Class CinematicTrigger
+
+} // Namespace RPG Cinematic
