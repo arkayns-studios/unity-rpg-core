881adebaac956399565b7f63e340c6769c4a07b9
Serge Maia <sergemaia9@gmail.com>
2022-01-31 11:17:03 +0000

    ADD: Loading Scenes & Changed script folder structure

---
+++ RPG Core Combat Creator/Assets/Scripts/RPG/SceneManagement/Portal.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RPG.SceneManagement {
+    
+    public class Portal : MonoBehaviour {
+        [SerializeField] private int m_sceneToLoad = -1;
+        
+        private void OnTriggerEnter(Collider other) {
+            if(other.CompareTag("Player")) 
+                SceneManager.LoadScene(m_sceneToLoad);
+        } // OnTriggerEnter
+
+    } // Class Portal
+    
+} // Namespace RPG SceneManagement
+
